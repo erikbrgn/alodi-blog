8bb0567997ca34d1e33922ecb72876a23e05d9ae
Erik Berggren <erik.berggren@axis.com>
2022-07-22 11:20:40 +0200

    Convert to typescript in nextjs client and remove boilerplate

---
+++ nextjs-client/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Alodi | Blog</title>
+        <meta
+          name="description"
+          content="A blog about the development of Alodi"
+        />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
